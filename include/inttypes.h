57a8c4d4ac904812c995f26be5e928ca7aade281
xezon <pascalkross@gmail.com>
2017-11-12 16:51:09 +0100

    Add better int64 types for zorro

--- include/inttypes.h
+++ include/inttypes.h
@@ -4,26 +4,24 @@
 
 #ifdef ZORRO_LITE_C
 
-struct longlong
-{
-	unsigned long lo;
-	long hi;
-};
-
-struct ulonglong
-{
-	unsigned long lo;
-	unsigned long hi;
-};
-
 typedef char           int8_t;
 typedef short          int16_t;
 typedef int            int32_t;
-typedef longlong       int64_t;
 typedef unsigned char  uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int   uint32_t;
-typedef ulonglong      uint64_t;
+
+typedef struct sint64
+{
+	uint32_t lo;
+	int32_t hi;
+} int64_t;
+
+typedef struct uint64
+{
+	uint32_t lo;
+	uint32_t hi;
+} uint64_t;
 
 #else
 
